e8f0396cd4b8131b757c05b1dca7367e105a3f1a
Iker Ibarguren <ikerib@gmail.com>
2017-07-04 14:17:54 +0200

    Removed unnecesary files

--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
-import App from './component/app/App';
 import registerServiceWorker from './registerServiceWorker';
-import Server from "./component/servers/server";
-import Sidebar from "./component/sidebar/sidebar";
-import Table from "./component/table/table";
-import Container from "./component/container/container";
 import Main from "./component/main/main";
 
 ReactDOM.render(<Main />, document.getElementById('main'));
-// ReactDOM.render(<Server />, document.getElementById('divNavBar'));
-// ReactDOM.render(<Sidebar />, document.getElementById('sidebar'));
-// ReactDOM.render(<Table />, document.getElementById('root'));
-// ReactDOM.render(<Container selectedFs="/mnt/nfs/Aplik/.zfs/snapshot/"/>, document.getElementById('container'));
 
 registerServiceWorker();
