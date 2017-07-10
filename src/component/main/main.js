46cf62f5bcfc06a08e78d5d312d6e19330146466
Iker Ibarguren <ikerib@gmail.com>
2017-07-10 13:52:51 +0200

    Added zsf folder into to combo

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -3,6 +3,7 @@ import Server from "../servers/server";
 import Sidebar from "../sidebar/sidebar";
 import Table from "../table/table";
 import { FormControl, ControlLabel } from 'react-bootstrap';
+const axios = require('axios');
 
 class Main extends Component {
     constructor(props) {
@@ -10,6 +11,7 @@ class Main extends Component {
         this.state = {
             server: null,
             selectedFs: null,
+            snapshoots:[],
         };
         this.handleFsChange = this.handleFsChange.bind(this);
         this.handleServerChange = this.handleServerChange.bind(this);
@@ -24,21 +26,34 @@ class Main extends Component {
     handleFsChange(path) {
         if ((path!==null) && (path!==undefined)) {
             this.setState({selectedFs: path});
+            const url = 'http://localhost:9000/lssnapshoot?dir='+path;
+            console.log("URL: ");
+            console.log(url);
+            axios.get(url)
+                .then(res => {
+                    const response = res.data.map(obj => obj);
+                    console.log("response:");
+                    console.log(response);
+                    this.setState({ snapshoots:response });
+                    console.log("state snapshoots");
+                    console.log(this.state.snapshoots);
+                });
         }
     }
 
     render() {
         return (
             <body>
+
             <nav className="navbar navbar-inverse navbar-fixed-top">
                 <div className="container-fluid">
                     <div className="navbar-header">
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
                                 data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                             <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
+                            <span className="icon-bar"/>
+                            <span className="icon-bar"/>
+                            <span className="icon-bar"/>
                         </button>
                         <a className="navbar-brand">Project name</a>
                     </div>
@@ -69,10 +84,13 @@ class Main extends Component {
                             <div className="col-md-6">
                                 <ControlLabel>Spanshoot: </ControlLabel>
                                 <FormControl componentClass="select" placeholder="Type">
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
+                                    {
+                                        this.state.snapshoots.map((option, index) => {
+                                            console.log(index);
+                                            console.log(option);
+                                            return (<option key={index} value={option}>{option}</option>)
+                                        })
+                                    }
                                 </FormControl>
                             </div>
                         </div>
