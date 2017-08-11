91ecaa5e94315757798cd6361e34af543b32c896
Iker Ibarguren <ikerib@gmail.com>
2017-08-11 13:13:32 +0200

    Build-a eta config fitxategia zerbitzarian egin beharko lirateke config.js .gitignore-ra gehituz. TODO

--- src/config.js
+++ src/config.js
@@ -1,15 +1,16 @@
 
-// const myconfig = {
-//     BASE_URL:'http://172.28.64.15:3000/',
-//     API_URL: 'http://172.28.64.15:9000/',
-//     MOUNT_POINT: '/mnt/nfs/'
-// };
-
 const myconfig = {
-    BASE_URL:'http://localhost:3000/',
-    API_URL: 'http://localhost:9000/',
+    BASE_URL:'http://172.28.64.15:3000/',
+    API_URL: 'http://172.28.64.15:9000/',
     MOUNT_POINT: '/mnt/nfs/',
     INIT_PATH: '/mnt/nfs/Donibane'
 };
+
+// const myconfig = {
+//     BASE_URL:'http://localhost:3000/',
+//     API_URL: 'http://localhost:9000/',
+//     MOUNT_POINT: '/mnt/nfs/',
+//     INIT_PATH: '/mnt/nfs/Donibane'
+// };
 
 module.exports=myconfig;
